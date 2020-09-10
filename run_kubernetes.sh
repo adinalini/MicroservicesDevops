9d679e80db59d893d36173a0b95be662098f92c8
adinalini <adinalini2000@gmail.com>
2020-09-10 15:36:47 +0000

    Updated kubernetes and up_docker shell scripts

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=adinalini/udacity-microservices-devops
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+kubectl run dockerappbuild\
+    --image=$dockerpath\
+    --port=80 --labels app=dockerappbuild
+    
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward dockerappbuild 8000:80
